bf6440dc77088e60323e1f62d18686669e582a6a
esjo93 <anderson93@naver.com>
2022-08-30 16:47:25 +0900

    pointer practice

--- session1/day1/09_esjo/02_pointer/hello.c
+++ session1/day1/09_esjo/02_pointer/hello.c
@@ -1,17 +1,31 @@
 // hello.c
 #include <stdio.h>
 
-// extern: this function exists somewhere...
-int g1 = 20;
-static int s1=14;
-const int c1 = 100; // no need to be assigned to memory...
-extern int increment(int i);
+int update(int* a, int* b){
+    *a = 123;
+    *b = 12;
+}
+
 
 int main() {
-    c1 = 12;
-    int i = g1 +c1;
-    printf("Hello, world! %d \n", increment(10));
-    printf("Hello, world! %d \n", increment(10));
+    int a = 10;
+    int b = 3;
+    int* ap = &a;
+    printf("a is allocated with %d bytes @ %p\n", sizeof(a), &a);
+    printf("a is %d @ %p\n", *ap, ap);
+
+    int** app = &ap;
+    update(&a, &b);
+    printf("a, b is %d, %d \n", a, b);
+
+    unsigned int mem = 0x12345678;
+
+    unsigned int* mp = &mem;
+    unsigned char* mbp = (unsigned char*)mp;
+    printf("%p | %2X\n", mbp+0, *(mbp+0));
+    printf("%p | %2X\n", mbp+1, *(mbp+1));
+    printf("%p | %2X\n", mbp+2, *(mbp+2));
+    printf("%p | %2X\n", mbp+3, *(mbp+3));
     return 0;
 }
 
