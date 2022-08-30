f6b3b93eaf3d93258ed9d1f23237050d016036b5
LetsGitItt <guh4411@gmail.com>
2022-08-30 13:50:06 +0900

    PushTest

---
+++ session1/day1/15_yeonghun/00_hello/hello.c
@@ -0,0 +1,20 @@
+// hello.c
+#include <stdio.h>
+ 
+extern int increment(int i);
+
+
+int main() {
+    int i=10;
+    printf("Hello, world! %d\n", increment(i));
+    return 0;
+}
+
+/*
+int increment(int i)
+{
+    return i+1;
+}*/
+
+
+
