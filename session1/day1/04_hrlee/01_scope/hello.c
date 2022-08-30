4c1ecaae43df19b6cd89416e2d610231fc9074ed
hyerim-mmc <rim7951@hanyang.ac.kr>
2022-08-30 15:16:38 +0900

    variable liveness

--- session1/day1/04_hrlee/01_scope/hello.c
+++ session1/day1/04_hrlee/01_scope/hello.c
@@ -1,9 +1,11 @@
 // hello.c
 #include <stdio.h>
+#define KK 200
 
 int g1=20;
 static int s1=14;                   // 다른 파일에서 접근 불가능  
 const int c1=100;
+int k1=KK;
 extern int increment(int i);        // 모든 파일에서 접근 가능
 
 int main(){
