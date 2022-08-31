612d3393c6aeb8566a02ec720a31f58670378a91
kjs <john042197@naver.com>
2022-08-31 16:23:16 +0900

    16:25

--- session1/day2/14_kjs/06_struct/hello.c
+++ session1/day2/14_kjs/06_struct/hello.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 void mem_inspection(unsigned char* p, int N){
-    for(int i =0; i<N; i++)
-        printf("mem[%d] is 0x%02X at %p \n", i, *(p+i), p+i);
+    for(int i = 0; i< N; i++)
+        printf("mem[%d] is 0x%02X at %p\n", i, *(p+i), p+i); // *(p+i) == p[i]
 }
-void main(){
-    struct bits_8 {
+
+void main() {
+    struct bits_8{
         unsigned char b0 : 1;
         unsigned char b1 : 1;
         unsigned char b2 : 1;
@@ -16,19 +17,41 @@ void main(){
         unsigned char b6 : 1;
         unsigned char b7 : 1;
     };
-
+    struct ADC_CONFIG{
+        unsigned char CNFG1;
+        unsigned char CNFG2;
+        unsigned short MODE;
+    };
+    union flag_16bits{
+        unsigned char bytes[2];
+        struct bits_8 b[2];
+        unsigned short bits16;
+    };
     struct ADC_CONFIG adc1 = {0x01, 0x80, 0xF000};
-    printf("CNFG1 : 0x%02X\n", adc1.CNFG1);
-    printf("CNFG2 : 0x%02X\n", adc1.CNFG1);
-    printf("MODE : 0x%04X\n", adc1.MODE);
+    printf("CNFG1: 0x%02X\n", adc1.CNFG1);
+    printf("CNFG2: 0x%02X\n", adc1.CNFG2);
+    printf("MODE: 0x%04X\n", adc1.MODE);
 
     unsigned char* adc_p = (unsigned char*)&adc1;
     mem_inspection(adc_p, sizeof(adc1));
 
-    *(adc_p+1) = 0x5A;
-    printf("CNFG2 : 0x%02X\n", adc1.CNFG2);
+    *(adc_p + 1) = 0x5A; // write CNFG2
+    printf("CNFG2: 0x%02X\n", adc1.CNFG2);
 
-    printf("struct bits_8 is aloocated with %d bytes\n", sizeof(bits_8));
+    struct bits_8 bytes;
+    printf("struct bits_8 is allocated with %d bytes\n", sizeof(bytes));
 
+    unsigned char b8 = 0xB;
+    // bytes = 0x80; // try to set MSB --> overwrite all --> error
+    b8 = 0x80;
+    printf("bytes: %02X\n", b8);
 
+    union flag_16bits flag16;
+    printf("union flag_16 is %d bytes\n", sizeof(flag16));
+
+    flag16.bits16 = 0xF005;
+    printf("flag16 is 0x%02X%02X\n", flag16.bytes[1], flag16.bytes[0]);
+
+    flag16.b[1].b7 = 0;
+    printf("flag16 is 0x%02X%02X\n", flag16.bytes[1], flag16.bytes[0]);
 }
