4b6045c9516cf518429871480ee96b00c2b6ad95
HONGGYU <ghdrb9138@naver.com>
2022-08-31 09:17:53 +0900

    8/31 start

--- session1/day1/20_hglee/02_pointer/hello.c
+++ session1/day1/20_hglee/02_pointer/hello.c
@@ -28,6 +28,10 @@ int main() {
     printf("%p | %2X\n",(mbp+2), *(mbp+2));
     printf("%p | %2X\n",(mbp+3), *(mbp+3));
 
+    int arr[5] = {1,2,3,4,5};
+    for (int i = 0; i<5; i++){
+        printf("arr[%d] is %d at %p \n", i, arr[i], &arr[i]);
+    }
 
     return 0;
 }
