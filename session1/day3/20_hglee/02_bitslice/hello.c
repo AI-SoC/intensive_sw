db5e5daf039f854979bdb3c06b1df58dbae53e29
HONGGYU <ghdrb9138@naver.com>
2022-09-01 14:17:20 +0900

    bitslice~

---
+++ session1/day3/20_hglee/02_bitslice/hello.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+struct BITS8
+{
+    /* data */
+    unsigned char b0 : 1;
+    unsigned char b1 : 1;
+    unsigned char b2 : 1;
+    unsigned char b3 : 1;
+    unsigned char b4 : 1;
+    unsigned char b5 : 1;
+    unsigned char b6 : 1;
+    unsigned char b7 : 1;
+
+};
+
+union PORT0{
+    unsigned char   U;
+    struct BITS8    B;    
+};
+
+
+int main(){
+    unsigned char P0 = 0x95;    // 10010101
+
+    P0 = P0 & ~(1<<4);          // 10000101 - 4번째 지우기
+    // printf("P0 1 : 0x%02X\n", P0);
+
+    P0 = P0 & ~(0x80);          // 10000101 - 4번째 지우기
+    // printf("P0 2 : 0x%02X\n", P0);
+
+
+    union PORT0 port0;
+    port0.U = 0xFF;
+    port0.B.b4 = 0;
+    printf("Port0 : 0x%02X\n", port0.U);
+
+    return 0;
+};
